e71ebe0a3caac33d330b175d0d1326d668b020aa
Per Orest <per.orest@outlook.com>
2019-09-14 20:13:38 +0200

    allmodels...

---
+++ Models/ViewModels/Reporting ViewModels/OrganizationReportViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBSPRO.Models.ViewModels.Reporting_ViewModels
+{
+    public class OrganizationReportViewModel
+    {
+    }
+}
