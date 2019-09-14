faf9b02d9f27180a0f5a266a0a08196a141fea81
Per Orest <per.orest@outlook.com>
2019-09-14 20:09:30 +0200

    All ViewModels Added

---
+++ Models/ViewModels/AccountingViewModels/PaymentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBSPRO.Models.ViewModels.AccountingViewModels
+{
+    public class PaymentViewModel
+    {
+    }
+}
---
+++ Models/ViewModels/ReportingViewModels/ReportViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBSPRO.Models.ViewModels.ReportingViewModels
+{
+    public class ReportViewModel
+    {
+    }
+}
